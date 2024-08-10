781f279e3dd49c2ffbfd82d5578cef546202e46a
Salmaan Saeed <salsaeed999@gmail.com>
2024-08-10 14:59:57 -0400

    added the header guards

--- src/keybindings/keybindings.hpp
+++ src/keybindings/keybindings.hpp
@@ -1,5 +1,10 @@
+#ifndef KEYBINDINGS_H
+#define KEYBINDINGS_H
+
 #include "engine/entities.hpp"
 
 bool clickKeybindings(SDL_Event event, MotionState *motionState, CollisionState *collisionState);
 
 void holdKeybindings(Player *player, SDL_GameController *gamecontroller);
+
+#endif // KEYBINDINGS_H
